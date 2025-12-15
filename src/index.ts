a631fc2a98f2670f70dee7ee17b4955e20411855
AJ Roetker <ajroetker@antfly.io>
2025-12-14 16:55:55 -0800

    Update rag types

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import './style.css'
-import ActiveFilters from './ActiveFilters'
-import AnswerFeedback from './AnswerFeedback'
-import AnswerResults from './AnswerResults'
-import { useAnswerResultsContext } from './AnswerResultsContext'
-import Antfly from './Antfly'
+import "./style.css";
+import ActiveFilters from "./ActiveFilters";
+import AnswerFeedback from "./AnswerFeedback";
+import AnswerResults from "./AnswerResults";
+import { useAnswerResultsContext } from "./AnswerResultsContext";
+import Antfly from "./Antfly";
 import Autosuggest, {
   AutosuggestFacets,
   AutosuggestResults,
   useAutosuggestContext,
-} from './Autosuggest'
-import CustomWidget from './CustomWidget'
+} from "./Autosuggest";
+import CustomWidget from "./CustomWidget";
 import {
   getCitedDocumentIds,
   getCitedResourceIds,
@@ -17,17 +17,21 @@ import {
   renderAsMarkdownLinks,
   renderAsSequentialLinks,
   replaceCitations,
-} from './citations'
-import Facet from './Facet'
-import { renderNumeric, renderStars, renderThumbsUpDown } from './feedback-renderers'
-import { useAnswerStream } from './hooks/useAnswerStream'
-import { useCitations } from './hooks/useCitations'
-import { useSearchHistory } from './hooks/useSearchHistory'
-import Listener from './Listener'
-import Pagination from './Pagination'
-import QueryBox from './QueryBox'
-import RAGResults, { useRAGResultsContext } from './RAGResults'
-import Results from './Results'
+} from "./citations";
+import Facet from "./Facet";
+import {
+  renderNumeric,
+  renderStars,
+  renderThumbsUpDown,
+} from "./feedback-renderers";
+import { useAnswerStream } from "./hooks/useAnswerStream";
+import { useCitations } from "./hooks/useCitations";
+import { useSearchHistory } from "./hooks/useSearchHistory";
+import Listener from "./Listener";
+import Pagination from "./Pagination";
+import QueryBox from "./QueryBox";
+import RAGResults, { useRAGResultsContext } from "./RAGResults";
+import Results from "./Results";
 import {
   fromUrlQueryString,
   getAntflyClient,
@@ -36,7 +40,7 @@ import {
   streamAnswer,
   streamRAG,
   toUrlQueryString,
-} from './utils'
+} from "./utils";
 
 export {
   Antfly,
@@ -75,7 +79,7 @@ export {
   useSearchHistory,
   useAnswerStream,
   useCitations,
-}
+};
 
 export type {
   AnswerAgentRequest,
@@ -83,28 +87,32 @@ export type {
   GeneratorConfig,
   RAGRequest,
   RAGResult,
-  SummarizeResult,
-} from '@antfly/sdk'
-export type { ActiveFilter, ActiveFiltersProps } from './ActiveFilters'
-export type { AnswerFeedbackProps, FeedbackResult } from './AnswerFeedback'
-export type { AnswerResultsProps } from './AnswerResults'
-export type { AnswerResultsContextValue } from './AnswerResultsContext'
+  GenerateResult,
+} from "@antfly/sdk";
+export type { ActiveFilter, ActiveFiltersProps } from "./ActiveFilters";
+export type { AnswerFeedbackProps, FeedbackResult } from "./AnswerFeedback";
+export type { AnswerResultsProps } from "./AnswerResults";
+export type { AnswerResultsContextValue } from "./AnswerResultsContext";
 // Export types for users of the library
-export type { AntflyProps } from './Antfly'
+export type { AntflyProps } from "./Antfly";
 export type {
   AutosuggestContextValue,
   AutosuggestFacetsProps,
   AutosuggestProps,
   AutosuggestResultsProps,
-} from './Autosuggest'
-export type { CustomWidgetProps } from './CustomWidget'
-export type { Citation, CitationRenderOptions } from './citations'
-export type { FacetProps } from './Facet'
-export type { QueryClassification } from './hooks/useAnswerStream'
-export type { CitationMetadata, SearchHistory, SearchResult } from './hooks/useSearchHistory'
-export type { PaginationProps } from './Pagination'
-export type { CustomInputProps, QueryBoxProps } from './QueryBox'
-export type { RAGResultsProps } from './RAGResults'
-export type { ResultsProps } from './Results'
-export type { SharedAction, SharedState, Widget } from './SharedContext'
-export type { AnswerCallbacks, MultiqueryRequest, RAGCallbacks } from './utils'
+} from "./Autosuggest";
+export type { CustomWidgetProps } from "./CustomWidget";
+export type { Citation, CitationRenderOptions } from "./citations";
+export type { FacetProps } from "./Facet";
+export type { QueryClassification } from "./hooks/useAnswerStream";
+export type {
+  CitationMetadata,
+  SearchHistory,
+  SearchResult,
+} from "./hooks/useSearchHistory";
+export type { PaginationProps } from "./Pagination";
+export type { CustomInputProps, QueryBoxProps } from "./QueryBox";
+export type { RAGResultsProps } from "./RAGResults";
+export type { ResultsProps } from "./Results";
+export type { SharedAction, SharedState, Widget } from "./SharedContext";
+export type { AnswerCallbacks, MultiqueryRequest, RAGCallbacks } from "./utils";
